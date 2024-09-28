028969568e8e4cd1cd8d2a7ac4887922e3c23e7c
Gilson Rodrigues de Almeida <83088082+GilsonRodriguesAlmeida@users.noreply.github.com>
2024-09-28 16:18:59 +0000

    teste de server rodando

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 import express from "express";
 
 const app = express();
+
+app.get("/usuarios", (request, response) => {
+  response.send("Ok, deu boa!");
+  console.log("server rodando...");
+});
+
+app.listen(process.env.PORT);
